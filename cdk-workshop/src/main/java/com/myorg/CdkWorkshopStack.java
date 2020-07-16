839dc0ad43165d3199ff76f4646d8ff7ad5c3510
Brandon S. Lee <acidzazz@gmail.com>
2020-07-16 14:39:48 +0900

    SQS removed, lambda added

--- cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
+++ cdk-workshop/src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,12 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Function.Builder;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -16,14 +16,14 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
-                .build();
-
-        final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+            .runtime(Runtime.NODEJS_10_X)
+            // The handler code is loaded from the lambda directory which we created earlier. Path is relative to where you execute cdk from, which is the project’s root directory
+            .code(Code.fromAsset("lambda"))
+            // The name of the handler function is hello.handler (“hello” is the name of the file and “handler” is the exported function name)
+            .handler("hello.handler")
             .build();
 
-        topic.addSubscription(new SqsSubscription(queue));
+        
     }
 }
